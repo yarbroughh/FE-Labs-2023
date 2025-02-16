322bfb41aae76cb975c206534766376880ea7792
yarbroughh <yarbroughh@gmail.com>
2025-02-16 12:18:07 -0500

    commit Lab07 solution

--- Lab06/JS3Lab.js
+++ Lab06/JS3Lab.js
@@ -40,6 +40,8 @@ let alphabetString = 'abcdefghijklmnopqrstuvwxyz';
  * 
  * ↓ YOUR CODE HERE ↓ */
 
+let alphaArray = alphabetString.split("");
+console.log(alphaArray);
 
 
 
@@ -59,9 +61,8 @@ let alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
-
-
+alphabetArray.reverse();
+console.log(alphabetArray.join(""));
 
 
 // Question 3: array1
@@ -82,7 +83,13 @@ let array1 = [1, 5, 6, 9, 10, 14];
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
+console.log(array1);
+console.log(array1[2]);
+console.log(array1[array1.length - 1]);
+array1.push(16, 3);
+console.log(array1);
+console.log(array1[2]);
+console.log(array1[array1.length - 1]);
 
 
 
@@ -102,8 +109,15 @@ Question 4: Todo List \n`);
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
-
+let myTodoList = [];
+myTodoList.push("water plants", "wash dishes", "vacuum");
+console.log(myTodoList);
+myTodoList.splice(1, 1);
+console.log(myTodoList);
+let yourTodoList = ["clean bathroom", "shovel sidewalk"];
+console.log(yourTodoList);
+let ourTodoList = myTodoList.concat(yourTodoList);
+console.log(ourTodoList);
 
 
 
@@ -123,17 +137,23 @@ Question 5: isEven  \n`);
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
+function isEven(x) {
+       if (x % 2 === 0) {
+              return true;
+       } else {
+              return false;
+       }
+}
 
 
 
 /* ↑ YOUR CODE HERE ↑ */
 
 //! Sample test cases for the above isEven function (uncomment to test)
-//console.log(isEven(2));   //should return true
-//console.log(isEven(3));   //should return false
-//console.log(isEven(4));   //should return true
-//console.log(isEven(5));   //should return false
+console.log(isEven(2));   //should return true
+console.log(isEven(3));   //should return false
+console.log(isEven(4));   //should return true
+console.log(isEven(5));   //should return false
 
 
 
@@ -152,16 +172,22 @@ Question 6: addingMachine \n`);
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
+function addingMachine(x) {
+       let sum = 0;
+       for (let i = 0; i < x.length; i++) {
+              sum += x[i];
+       }
+       return sum;
+}
 
 
 
 /* ↑ YOUR CODE HERE ↑ */
 
 //! Sample test cases for the above isEven function (uncomment to test)
-//console.log(addingMachine([1, 2, 3, 4, 5]));                    //should return 15
-//console.log(addingMachine([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));    //should return 55
-//console.log(addingMachine([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])); //should return 120
+console.log(addingMachine([1, 2, 3, 4, 5]));                    //should return 15
+console.log(addingMachine([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));    //should return 55
+console.log(addingMachine([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])); //should return 120
 
 
 
@@ -185,17 +211,27 @@ Question 7: reverse \n`);
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
-
-
+function reverse(x) {
+       if (typeof x === "boolean") {
+              return !x;  
+       } else if (typeof x === "number") { 
+              return parseInt(x.toString().split("").reverse().join(""));
+       } else if (typeof x === "string") {
+              return x.split("").reverse().join("");    
+       } else if (Array.isArray(x)) {
+              return x.reverse();
+       } else {
+              return false;
+       }
+}
 
 /* ↑ YOUR CODE HERE ↑ */
 
 // Sample test cases for the above reverse function (uncomment to test)
-//console.log(reverse(1234));         //should return 4321
-//console.log(reverse('hello'));      //should return 'olleh'
-//console.log(reverse(true));         //should return false
-//console.log(reverse([1, 2, 3, 4])); //should return [4, 3, 2, 1]
+console.log(reverse(1234));         //should return 4321
+console.log(reverse('hello'));      //should return 'olleh'
+console.log(reverse(true));         //should return false
+console.log(reverse([1, 2, 3, 4])); //should return [4, 3, 2, 1]
 
 
 
@@ -218,14 +254,19 @@ Question 8: removeElements \n`);
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
+function removeElements(array) {
+       while (array.length > 0) {
+              array.pop();
+       } 
+       return(array);
+}
 
 
 
 /* ↑ YOUR CODE HERE ↑ */
 
 // Sample test cases for the above removeElements function (uncomment to test)
-//console.log(removeElements([1, 2, 3, 4, 5])); //should return [] (an empty array)
+console.log(removeElements([1, 2, 3, 4, 5])); //should return [] (an empty array)
 
 
 
@@ -234,7 +275,8 @@ console.log(`--------------------------
 Question 9: whichArrayIsLonger  \n`);
 
 /*
- * Step 1:  Create a function called whichArrayIsLonger. The purpose of this function is to determine which array has the most elements
+ * Step 1:  Create a function called whichArrayIsLonger. The purpose of this function is to determine 
+which array has the most elements
         * This function takes in two parameters (array1, array2)
         * You can use conditional statements to determine which array has the most elements
         * This function will return the array that has the most elements    
@@ -245,13 +287,21 @@ Question 9: whichArrayIsLonger  \n`);
  * 
  * ↓ YOUR CODE HERE ↓ */
 
-
+function whichArrayIsLonger(array1, array2) {
+       if (array1.length > array2.length) {
+              return array1
+       } else if (array2.length > array1.length) {
+              return array2
+       } else {
+              return false;
+       }
+}
 
 
 
 /* ↑ YOUR CODE HERE ↑ */
 
 // Sample test cases for the above whichArrayIsLonger function (uncomment to test)
-//console.log(whichArrayIsLonger([1, 2, 3, 4, 5], [1, 2, 3, 4]));    //should return [1, 2, 3, 4, 5]
-//console.log(whichArrayIsLonger([1, 2, 3, 4], [1, 2, 3, 4, 5, 6])); ///should return [1, 2, 3, 4, 5, 6]
-//console.log(whichArrayIsLonger([1, 2, 3, 4], [1, 2, 3, 4]));       //should return false (because they are the same length)
+console.log(whichArrayIsLonger([1, 2, 3, 4, 5], [1, 2, 3, 4]));    //should return [1, 2, 3, 4, 5]
+console.log(whichArrayIsLonger([1, 2, 3, 4], [1, 2, 3, 4, 5, 6])); ///should return [1, 2, 3, 4, 5, 6]
+console.log(whichArrayIsLonger([1, 2, 3, 4], [1, 2, 3, 4]));       //should return false (because they are the same length)
--- Lab07/JS4Lab.js
+++ Lab07/JS4Lab.js
@@ -39,13 +39,19 @@ console.log(`--------------------------
 Question 1: Declaring Let and Const variables`)
 
 /*
- * Step 1: Initialize two variables using 'const' and set the value of those variables to what you're having to eat and drink
- * Step 2: Initialize two more variables using 'let' and set one value to your name, and the other to your friends name.
+ * Step 1: Initialize two variables using 'const' and set the value of those variables
+ to what you're having to eat and drink
+ * Step 2: Initialize two more variables using 'let' and set one value to your name,
+  and the other to your friends name.
  *
  * ↓ YOUR CODE HERE ↓ */
+const eat="matar paneer";
+let drink="white wine";
+let me="Heather";
+let friend="Terri";
 
 //Question: Name a reason why you would use const instead of let.
-//Answer:
+//Answer: If you never want the value to change from what you initially assign it. e.g., I made matar paneer and have no other food prepared, so that's what we're eating.
 
 /*---------------------------------- Template Literals -------------------------------------*/
 console.log(`-------------------------- 
@@ -57,23 +63,28 @@ Question 2: Template Literals`)
  *
  *
  * ↓ YOUR CODE HERE ↓ */
-
+console.log(`${me} and ${friend} are having ${eat} and ${drink} for lunch.`)
 //Question: How would the sentence you wrote look if you used string concatenation instead of template literals?
 //          console.log() your answer to check
 //Answer:
+console.log(me + " and " + friend + " are having " + eat + " and " + drink + " for lunch.");
 console.log(`-------------------------- 
 Question 2a: Oh no`)
 
 /* Oh no! You just remembered your friend is allergic to the drink we prepared!
  *
- * Step 1: In the section below, reassign your drink item to something else. console.log() your drink variable and run your code.
- * Step 2: Make a change to your code in Question 1 to fix the error. Run your code to make sure it's fixed.
+ * Step 1: In the section below, reassign your drink item to something else. console.log()
+ *  your drink variable and run your code.
+ * Step 2: Make a change to your code in Question 1 to fix the error. Run your code to 
+ * make sure it's fixed.
  *
  * ↓ YOUR CODE HERE ↓ */
+drink="chai tea";
+console.log(drink);
 
 //Question: Read the error message - in your own words - what does it mean?
 //Answer:
-
+//We originally had drink set as a const which assigns a value that cannot be changed so we received error "Assignment to constant variable" 
 /*---------------------------------- Arrow Functions -------------------------------------*/
 console.log(`-------------------------- 
 Question 3: Arrow Functions`)
@@ -90,22 +101,35 @@ Question 3: Arrow Functions`)
  * Step 3: Copy the code below, ↓ paste it underneath our new function, and run the code.
  *         console.log(lunchTime('Pizza','Water'))
  * ↓ YOUR CODE HERE ↓ */
-
+const lunchTime=(foodChoice, drinkChoice) => {return `${me} and ${friend} are having ${foodChoice} and ${drinkChoice} for lunch.`};
+console.log(lunchTime('Pizza','Water'))
 //Question: What would your function look like if we were using the old way of creating a function?
 //Answer:
-
+function lunchTimeAgain(foodChoice, drinkChoice) {
+   console.log(me + " and " + friend + " are having " + foodChoice + " and " + drinkChoice + " for lunch.");
+}
+lunchTimeAgain("Pizza", "Water");
 /*---------------------------------- Callbacks -------------------------------------*/
 console.log(`-------------------------- 
 Question 4: Callbacks`)
 
 /**
- * You and your friend ALWAYS eat lunch, but it might be a good idea to plan for the next date in advance.
- * Let's use functions and callbacks to describe what we're eating today, and what we want tomorrow.
+ * You and your friend ALWAYS eat lunch, but it might be a good idea to plan for the next
+ *  date in advance.
+ * Let's use functions and callbacks to describe what we're eating today, and what we want
+ *  tomorrow.
  *
- * Step 1: Create a ES6 fat arrow function called 'foodForTodayAndTomorrow' that takes 3 parameters: tomorrowsFoodChoice, tomorrowsDrinkChoice, and callbackFunction.
- * Step 2: Have your function return a new string in this format: `${callbackFunction} Tomorrow we want ${tomorrowsFoodChoice} and ${tomorrowsDrinkChoice}.`
- * Step 3: console.log() your new function, foodForTodayAndTomorrow, and pass in lunchTime('newFoodItem', 'newDrinkItem') as the argument for the "callbackFunction" parameter.
+ * Step 1: Create a ES6 fat arrow function called 'foodForTodayAndTomorrow' that takes 3 
+ * parameters: tomorrowsFoodChoice, tomorrowsDrinkChoice, and callbackFunction.
+ * Step 2: Have your function return a new string in this format: `${callbackFunction} 
+ * Tomorrow we want ${tomorrowsFoodChoice} and ${tomorrowsDrinkChoice}.`
+ * Step 3: console.log() your new function, foodForTodayAndTomorrow, and pass in 
+ * lunchTime('newFoodItem', 'newDrinkItem') as the argument for the "callbackFunction"
+ *  parameter.
  *
  * ↓ YOUR CODE HERE ↓ */
-
+const foodForTodayAndTomorrow=(tomorrowsFoodChoice, tomorrowsDrinkChoice, callbackFunction) => {
+   return `${callbackFunction} Tomorrow we want ${tomorrowsFoodChoice} and ${tomorrowsDrinkChoice}.`;
+};
+console.log(foodForTodayAndTomorrow("lasagna", "red wine", lunchTime("matar paneer", "chai tea")));
 console.log(`-----------Finished------------`)
